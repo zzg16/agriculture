d6833bf1f032a6e1e31d31e31434e4867dfaabfb
西山渔夫 <zzg16@users.noreply.github.com>
2014-01-03 10:17:14 +0800

    Create models.py

---
+++ models.py
@@ -0,0 +1,26 @@
+#coding=UTF-8
+'''
+Created on Jan 3, 2014
+
+@author: zzg16@163.com
+'''
+from django.db import models
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
